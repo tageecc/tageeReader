c0b10a33c9ad24f0b74abfc88056d099cc9bc541
tagee <zhangzexuan@4399inc.com>
2017-02-09 19:33:02 +0800

    1

--- src/pages/Shelf/index.js
+++ src/pages/Shelf/index.js
@@ -6,19 +6,12 @@ import {
     NavigationExperimental,
     ScrollView,
     StyleSheet,
-    LayoutAnimation,
 } from 'react-native';
 
 import MainHeader from '../../components/header/MainHeader';
 import MainList from '../../components/MainList/index';
 import SideMenu from 'react-native-side-menu';
 import Menu from '../../components/Menu/index';
-import Reader from '../../pages/Reader/index';
-
-const {
-    Header: NavigationHeader,
-    CardStack: NavigationCardStack
-} = NavigationExperimental;
 
 export default class tageeReader extends React.Component {
 
@@ -28,9 +21,6 @@ export default class tageeReader extends React.Component {
             isOpen: false,
             key: 'HOME'
         };
-
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-
     }
 
     onMenuItemSelected(key) {
@@ -60,50 +50,13 @@ export default class tageeReader extends React.Component {
                 disableGestures={false}
                 menu={<Menu onMenuItemSelected={this.onMenuItemSelected.bind(this)} />}
             >
-                <NavigationCardStack
-                    onNavigate={ () => {} }
-                    navigationState={{ key: this.state.key, index: 0, routes: [{ key: this.state.key, title: 'My Initial Scene' }] }}
-                    renderScene={this._renderScene.bind(this)}
-                />
+                <View>
+                    <MainHeader
+                        title="首页"
+                        toggleMenu={this.toggleMenu.bind(this)}/>
+                    <MainList onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
+                </View>
             </SideMenu>
         );
-    }
-    _renderScene(props) {
-        const ComponentToRender = this._renderRoute(props.scene.route.key);
-        return (
-            <ScrollView>
-                {ComponentToRender}
-            </ScrollView>
-        );
-    }
-    _renderRoute(key) {
-        if (key === 'HOME') {
-            return <View>
-                <MainHeader
-                    title="首页"
-                    toggleMenu={this.toggleMenu.bind(this)}/>
-                <MainList  onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
-            </View>
-        }
-        if (key === 'ABOUT') {
-            return <View>
-                <MainHeader
-                    title="首页"
-                    toggleMenu={this.toggleMenu.bind(this)}/>
-                <MainList  onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
-            </View>
-        }
-        if (key === 'FEEDBACK') {
-            return <View>
-                <MainHeader
-                    title="首页"
-                    toggleMenu={this.toggleMenu.bind(this)}/>
-                <MainList  onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
-            </View>
-        }
-        if (key === 'READER') {
-
-            return <Reader />
-        }
     }
 }
--- src/root.js
+++ src/root.js
@@ -1,109 +1,59 @@
-import React from 'react';
-import {
-    View,
-    TouchableHighlight,
-    Text,
-    NavigationExperimental,
-    ScrollView,
-    StyleSheet,
-    LayoutAnimation,
-} from 'react-native';
 
-import MainHeader from './components/header/MainHeader';
-import MainList from './components/MainList/index';
-import SideMenu from 'react-native-side-menu';
-import Menu from './components/Menu/index';
-import Reader from './pages/Reader/index';
+import React, { Component, Navigator } from 'react-native';
+import { connect } from 'react-redux';
+import Router from './configs/router';
+import { skipLogin, asyncSkipLogin } from './actions/reader';
 
-const {
-    Header: NavigationHeader,
-    CardStack: NavigationCardStack
-} = NavigationExperimental;
+import ShelfPage from './pages/Shelf';
+import ReaderPage from './pages/Reader';
 
-export default class tageeReader extends React.Component {
+let initialRoute = {
+    name: 'shelf-page',
+    page: ShelfPage,
+};
 
-    constructor(props) {
+class Root extends Component {
+    constructor(props){
         super(props);
-        this.state = {
-            isOpen: false,
-            key: 'HOME'
-        };
-
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-
     }
 
-    onMenuItemSelected(key) {
-        this.setState({
-            isOpen: false,
-            key: key
-        })
+    renderScene({page, name, id, index, props}, navigator){
+        this.router = this.router || new Router(navigator);
+        if(page){
+            return React.createElement(page, {
+                ...props,
+                ref: view => this[index] = view,
+                router: this.router,
+                name,
+                route: {
+                    name,  id,  index
+                }
+            })
+        }
     }
 
-    toggleMenu() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-
-    setOpen(isOpen) {
-        this.setState({
-            isOpen: isOpen
-        })
+    configureScene(route){
+        if(route.sceneConfig){
+            return route.sceneConfig;
+        }
+        return Navigator.SceneConfigs.FloatFromRight;
     }
 
     render() {
-
-        return (
-            <SideMenu
-                isOpen={this.state.isOpen}
-                disableGestures={false}
-                menu={<Menu onMenuItemSelected={this.onMenuItemSelected.bind(this)} />}
-            >
-                <NavigationCardStack
-                    onNavigate={ () => {} }
-                    navigationState={{ key: this.state.key, index: 0, routes: [{ key: this.state.key, title: 'My Initial Scene' }] }}
-                    renderScene={this._renderScene.bind(this)}
-                />
-            </SideMenu>
-        );
-    }
-    _renderScene(props) {
-        const ComponentToRender = this._renderRoute(props.scene.route.key);
-        return (
-            <ScrollView>
-                {ComponentToRender}
-            </ScrollView>
-        );
-    }
-    _renderRoute(key) {
-        if (key === 'HOME') {
-            return <View>
-                <MainHeader
-                    title="首页"
-                    toggleMenu={this.toggleMenu.bind(this)}/>
-                <MainList  onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
-            </View>
-        }
-        if (key === 'ABOUT') {
-            return <View>
-                <MainHeader
-                    title="首页"
-                    toggleMenu={this.toggleMenu.bind(this)}/>
-                <MainList  onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
-            </View>
-        }
-        if (key === 'FEEDBACK') {
-            return <View>
-                <MainHeader
-                    title="首页"
-                    toggleMenu={this.toggleMenu.bind(this)}/>
-                <MainList  onMenuItemSelected={this.onMenuItemSelected.bind(this)}/>
-            </View>
-        }
-        if (key === 'READER') {
-
-            return <Reader />
-        }
+        return (<Navigator
+            ref={view => this.navigator=view}
+            initialRoute={initialRoute}
+            configureScene={this.configureScene.bind(this)}
+            renderScene={this.renderScene.bind(this)}
+        />);
     }
 }
+
+function select(store){
+    return {
+        isLoggedIn: store.userStore.isLoggedIn
+    }
+}
+
+
+export default connect(select)(Root);
